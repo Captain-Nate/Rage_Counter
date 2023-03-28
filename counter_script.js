4ee47c912a8f3f4f9fc3c7b56ff0d2c65bed381d
Nathaniel Mason <129145732+Captain-Nate@users.noreply.github.com>
2023-03-28 03:29:08 -0700

    Counter Done
    Everything should be working now!

---
+++ counter_script.js
@@ -0,0 +1,27 @@
+const inc_btn = document.getElementById("increase_btn");
+const dec_btn = document.getElementById("decrease_btn");
+const res_btn = document.getElementById("reset_btn");
+
+let curr_val = document.getElementById("rage_count").innerHTML;
+
+function update_num() {
+    document.getElementById("rage_count").innerHTML = curr_val;
+};
+
+inc_btn.addEventListener("click", function() {
+    curr_val++;
+    update_num();
+});
+
+dec_btn.addEventListener("click", function() {
+    if(curr_val > 0){
+        curr_val--;
+        update_num();
+    }
+});
+
+res_btn.addEventListener("click", function() {
+    curr_val=10;
+    update_num();
+});
+
